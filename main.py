3ac4db886cd371e605958ff76d8646b81cc2f6e3
Udita Kausakhi U <udaudita@gmail.com>
2024-03-03 19:30:49 +0530

    stage_02

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from TumorClassifier import logger
 from TumorClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from TumorClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -8,6 +9,18 @@ try :
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed \n\n x===============x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare Base Model"
+try :
+
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>> stage {STAGE_NAME} completed \n\n x===============x")
 
 except Exception as e:
--- src/TumorClassifier/entity/config_entity.py
+++ src/TumorClassifier/entity/config_entity.py
@@ -10,3 +10,14 @@ class DataIngestionConfig:
     non_tumor_local_file: Path
     golima_local_file: Path
     unzip_dir: Path
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
